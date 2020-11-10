bc77af0b521281370835eed46b5b366fe6dcf0c0
Vijaykumar2021 <lettertovijaykumar@gmail.com>
2020-11-10 20:40:31 +0530

    Refactored: Changed default message

--- conflict.sh
+++ conflict.sh
@@ -2,6 +2,6 @@ diceNum=$(( RANDOM%6 + 1 ))
 case $diceNum in 
 		1) echo "This is Ist face"
 			;;
-		*) echo "Thid is Default face"
+		*) echo "Thid is Default face which will execute if no above cases are true"
 			;;
 esac
